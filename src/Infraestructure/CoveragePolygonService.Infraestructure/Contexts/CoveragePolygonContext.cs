9971f0ce1a24ffaac7af1e3e707773126b1998bd
Carlos Dz <cdaza@infotransgroup.com>
2022-04-06 21:39:56 -0500

    Se realiza implementación de entidades e implementación de EntityFramework.

---
+++ src/Infraestructure/CoveragePolygonService.Infraestructure/Contexts/CoveragePolygonContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using CoveragePolygonService.Core.Entities;
+
+namespace CoveragePolygonService.Infraestructure.Contexts
+{
+    public class CoveragePolygonContext : DbContext
+    {
+        public DbSet<RouteCoverage> RouteCoverages { get; set; }
+        public DbSet<Geoposition> Geopositions { get; set; }
+        public CoveragePolygonContext(DbContextOptions<CoveragePolygonContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/Infraestructure/CoveragePolygonService.Infraestructure/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using CoveragePolygonService.Infraestructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CoveragePolygonService.Infraestructure
+{
+    public static class Extensions
+    {
+        private static readonly string CoveragePolygonConnectionString = "CoveragePolygon";
+        public static IServiceCollection AddInfraestructureServices(this IServiceCollection services)
+        {
+            IConfiguration configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+
+            services.AddDbContext<CoveragePolygonContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString(CoveragePolygonConnectionString)));
+
+            return services;
+        }
+    }
+}
